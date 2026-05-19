3304ec2ec0b319a83208d094ae71405ede2742e1
JhoeJhl <pinto.12574531@gmail.com>
2026-05-19 16:44:24 -0400

    Configuracion del run.py para render

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-
+import os
 #Importacion de controladores
 from controllers import MedicoController
 from controllers import PacienteController
@@ -34,6 +34,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port = int(os.environ.get('PORT', 5000)))
 
 
